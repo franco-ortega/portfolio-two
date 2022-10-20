ac470711440f4925d69e8eaff53b1518c22fe207
Franco Ortega <francopdx@gmail.com>
2022-10-20 16:10:41 -0700

    add filler content to Home page

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,7 @@
 export default function Home() {
   return (
     <div>
-      {/* <Head>
-        <title>Portfolio: Franco Ortega - Software Engineer</title>
-        <meta
-          name='description'
-          content='Portfolio site for Franco Ortega - Software Engineer'
-        />
-        <link rel='icon' href='/favicon.ico' />
-      </Head> */}
+      <h2>Home Page</h2>
     </div>
   );
 }
