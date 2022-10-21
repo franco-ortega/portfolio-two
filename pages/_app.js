d500be3f9118d5395728707b1826e5b81ac420c8
Franco Ortega <francopdx@gmail.com>
2022-10-21 12:26:15 -0700

    display Landing component first

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import { useState } from 'react';
+import Landing from '../components/landing/Landing';
 import Layout from '../components/layout/Layout';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  const [landing, setLanding] = useState(true);
+
+  if (landing) return <Landing />;
+
   return (
     <Layout>
       <Component {...pageProps} />
