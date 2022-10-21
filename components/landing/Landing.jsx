994f7b1d5f86b04b02c61ba3eadd61586457706e
Franco Ortega <francopdx@gmail.com>
2022-10-21 12:55:30 -0700

    add styles to Landing component

--- components/landing/Landing.jsx
+++ components/landing/Landing.jsx
@@ -1,11 +1,14 @@
 import Button from '../buttons/Button';
+import styles from './Landing.module.scss';
 
 const Landing = ({ handler }) => {
   return (
-    <div>
-      <h1>Franco Ortega</h1>
-      <h2>Software Engineer</h2>
-      <Button handler={handler} text={'Welcome'} />
+    <div className={styles.Landing}>
+      <div>
+        <h1>Franco Ortega</h1>
+        <h2>Software Engineer</h2>
+        <Button handler={handler} text={'Welcome'} />
+      </div>
     </div>
   );
 };
