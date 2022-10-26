e6ee97d47c8170ddf816cda9ef5315446bba882b
Franco Ortega <francopdx@gmail.com>
2022-10-26 12:09:51 -0700

    create FadeIn class for Landing component

--- components/landing/Landing.jsx
+++ components/landing/Landing.jsx
@@ -2,8 +2,13 @@ import Button from '../buttons/Button';
 import styles from './Landing.module.scss';
 
 const Landing = ({ handler, fadeOut }) => {
+  console.log({ fadeOut });
   return (
-    <div className={`${styles.Landing} ${fadeOut && styles.FadeOut}`}>
+    <div
+      className={`${styles.Landing} ${fadeOut && styles.FadeOut} ${
+        styles.FadeIn
+      }`}
+    >
       <div>
         <h1>Franco Ortega</h1>
         <h2>Software Engineer</h2>
