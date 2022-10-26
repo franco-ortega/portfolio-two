4e70bab4a289358ab509ce1e379d1b571311fcce
Franco Ortega <francopdx@gmail.com>
2022-10-26 11:48:56 -0700

    move Landing page logic into Layout component

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,7 +1,35 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import Landing from '../landing/Landing';
 import styles from './Layout.module.scss';
 
 const Layout = ({ children }) => {
+  const [landing, setLanding] = useState(true);
+  const [fadeOut, setFadeOut] = useState(false);
+  const [noPreference, setNoPreference] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    setNoPreference(
+      window.matchMedia('(prefers-reduced-motion: no-preference)').matches
+    );
+
+    if (router.pathname !== '/') setLanding(false);
+  }, [router]);
+
+  const onWelcomeClick = () => {
+    if (noPreference) setFadeOut(true);
+    setTimeout(
+      () => {
+        setLanding(false);
+      },
+      noPreference ? 1000 : 0
+    );
+  };
+
+  if (landing) return <Landing handler={onWelcomeClick} fadeOut={fadeOut} />;
+
   return (
     <div className={styles.Layout} data-testid='layout'>
       <Head>
--- pages/_app.js
+++ pages/_app.js
@@ -1,35 +1,7 @@
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import Landing from '../components/landing/Landing';
 import Layout from '../components/layout/Layout';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  const [landing, setLanding] = useState(true);
-  const [fadeOut, setFadeOut] = useState(false);
-  const [noPreference, setNoPreference] = useState(false);
-  const router = useRouter();
-
-  useEffect(() => {
-    setNoPreference(
-      window.matchMedia('(prefers-reduced-motion: no-preference)').matches
-    );
-
-    if (router.pathname !== '/') setLanding(false);
-  }, [router]);
-
-  const onWelcomeClick = () => {
-    if (noPreference) setFadeOut(true);
-    setTimeout(
-      () => {
-        setLanding(false);
-      },
-      noPreference ? 1000 : 0
-    );
-  };
-
-  if (landing) return <Landing handler={onWelcomeClick} fadeOut={fadeOut} />;
-
   return (
     <Layout>
       <Component {...pageProps} />
