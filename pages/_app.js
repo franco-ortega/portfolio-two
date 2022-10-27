e425f52824f9519461ddb7a9ede0f24313b5179c
Franco Ortega <francopdx@gmail.com>
2022-10-27 15:20:03 -0700

    import Font Awesome into _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,6 @@
+import '@fortawesome/fontawesome-svg-core/styles.css'; // import Font Awesome CSS
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 import App from '../components/app/App';
 import '../styles/globals.css';
 
