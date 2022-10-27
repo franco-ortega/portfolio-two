a900f25af72ae5decaf5cdda4e4e18795cee92f0
Franco Ortega <francopdx@gmail.com>
2022-10-27 16:28:40 -0700

    remove font-size from SocialLink

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -4,9 +4,6 @@ import styles from './Layout.module.scss';
 const Layout = ({ children }) => {
   return (
     <div className={styles.Layout} data-testid='layout'>
-      <header>
-        <h1>Franco Ortega - Software Engineer</h1>
-      </header>
       <Nav />
       <main>{children}</main>
     </div>
