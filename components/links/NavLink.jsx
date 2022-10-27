d8a29e764d2c7050e3772a027c4e29314be183d0
Franco Ortega <francopdx@gmail.com>
2022-10-27 15:32:15 -0700

    create styles for SocialLink

--- components/links/NavLink.jsx
+++ components/links/NavLink.jsx
@@ -1,7 +1,12 @@
 import Link from 'next/link';
+import styles from 'NavLink.module.scss';
 
 const NavLink = ({ text, url }) => {
-  return <Link href={url}>{text}</Link>;
+  return (
+    <Link className={styles.NavLink} href={url}>
+      {text}
+    </Link>
+  );
 };
 
 export default NavLink;
--- components/links/SocialLink.jsx
+++ components/links/SocialLink.jsx
@@ -1,8 +1,15 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from './SocialLink.module.scss';
 
 const SocialLink = ({ title, url, icon, iconStyle }) => {
   return (
-    <a title={title} href={url} rel='noreferrer' target={'blank'}>
+    <a
+      className={styles.SocialLink}
+      title={title}
+      href={url}
+      rel='noreferrer'
+      target={'blank'}
+    >
       <FontAwesomeIcon icon={icon} style={iconStyle} />
     </a>
   );
