6320a2f4d8a7f88ad31397f138c13bdc90f0230a
Franco Ortega <francopdx@gmail.com>
2022-10-30 20:11:14 -0700

    update Home component elements

--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -1,23 +1,27 @@
+import styles from './Home.module.scss';
+
 const Home = () => {
   return (
-    <div data-testid='home'>
-      <div>
+    <div className={styles.Home} data-testid='home'>
+      <p>
         I&apos;m a software engineer who loves to create intuitive interfaces
         and build out features across the stack. I value:
-        <ul>
-          <li>
-            well-documented and efficient processes that offer both structure
-            and flexibility
-          </li>
-          <li>clear and kind communication</li>
-          <li>cross-functional collaboration</li>
-          <li>
-            supportive environments that foster diversity and nurture growth
-          </li>
-        </ul>
+      </p>
+      <ul>
+        <li>
+          well-documented and efficient processes that offer both structure and
+          flexibility
+        </li>
+        <li>clear and kind communication</li>
+        <li>cross-functional collaboration</li>
+        <li>
+          supportive environments that foster diversity and nurture growth
+        </li>
+      </ul>
+      <p>
         Optimism guides my efforts while practicality ensures that my goals are
         brought to fruition, and helping others brings me great joy.
-      </div>
+      </p>
     </div>
   );
 };
