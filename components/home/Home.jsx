42d644b47dca2f2e44b2a05e8df50486fc2223bf
Franco Ortega <francopdx@gmail.com>
2022-10-30 20:05:25 -0700

    create Home component

---
+++ components/home/Home.jsx
@@ -0,0 +1,25 @@
+const Home = () => {
+  return (
+    <div data-testid='home'>
+      <div>
+        I&apos;m a software engineer who loves to create intuitive interfaces
+        and build out features across the stack. I value:
+        <ul>
+          <li>
+            well-documented and efficient processes that offer both structure
+            and flexibility
+          </li>
+          <li>clear and kind communication</li>
+          <li>cross-functional collaboration</li>
+          <li>
+            supportive environments that foster diversity and nurture growth
+          </li>
+        </ul>
+        Optimism guides my efforts while practicality ensures that my goals are
+        brought to fruition, and helping others brings me great joy.
+      </div>
+    </div>
+  );
+};
+
+export default Home;
