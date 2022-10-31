7cc1baa80a27bffd5087388cdd75dca311f59727
Franco Ortega <francopdx@gmail.com>
2022-10-31 16:17:12 -0700

    implement logic for applying dark mode

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import Nav from '../navs/Nav';
 import styles from './Layout.module.scss';
 
 const Layout = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const onDarkModeClick = () => setDarkMode((prevState) => !prevState);
+
   return (
-    <div className={styles.Layout} data-testid='layout'>
+    <div
+      className={`${styles.Layout} ${darkMode && styles.DarkMode}`}
+      data-testid='layout'
+    >
       <Nav />
+      <button onClick={onDarkModeClick}>DARK</button>
       <main>{children}</main>
     </div>
   );
