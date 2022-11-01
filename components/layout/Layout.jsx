a9d5b32ac9488fb419c7fbe7fbec9f51fc98ff2e
Franco Ortega <francopdx@gmail.com>
2022-10-31 17:07:43 -0700

    clean up code

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,17 +1,12 @@
 import { useState } from 'react';
-import DarkButton from '../buttons/DarkButton';
-
 import Nav from '../navs/Nav';
+import DarkButton from '../buttons/DarkButton';
 import styles from './Layout.module.scss';
 
 const Layout = ({ children }) => {
   const [darkMode, setDarkMode] = useState(false);
 
-  const onDarkModeClick = () => {
-    console.log('dark!!');
-    setDarkMode((prevState) => !prevState);
-  };
-
+  const onDarkModeClick = () => setDarkMode((prevState) => !prevState);
   return (
     <div
       className={`${styles.Layout} ${darkMode && styles.DarkMode}`}
