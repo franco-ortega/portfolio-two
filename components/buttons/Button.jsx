7f539c9f7ab1aaf9bdc7507a822aa811ff05c84d
Franco Ortega <francopdx@gmail.com>
2022-11-01 22:38:42 -0700

    refactor DarkButton to use CSS module for screen size placement and dark mode styles

--- components/buttons/Button.jsx
+++ components/buttons/Button.jsx
@@ -1,8 +1,8 @@
 import styles from './Button.module.scss';
 
-const Button = ({ handler, customStyles, text }) => {
+const Button = ({ customStyles, handler, text }) => {
   return (
-    <button className={styles.Button} style={customStyles} onClick={handler}>
+    <button className={`${styles.Button} ${customStyles}`} onClick={handler}>
       {text}
     </button>
   );
--- components/buttons/DarkButton.jsx
+++ components/buttons/DarkButton.jsx
@@ -1,23 +1,25 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoon } from '@fortawesome/free-solid-svg-icons';
 import Button from './Button';
+import styles from './DarkButton.module.scss';
 
 const DarkButton = ({ handler, darkMode, isMatch }) => {
-  const customStyles = {
-    backgroundColor: darkMode ? 'white' : 'black',
-    borderWidth: '2rem',
-    border: 'none',
-    borderRadius: '100vmax',
-    padding: '0.5rem .65rem',
-    position: 'absolute',
-    top: isMatch ? '0.5rem' : '3.25rem',
-    right: '.5rem'
-  };
+  console.log({ isMatch });
+  // const customStyles = {
+  //   backgroundColor: darkMode ? 'white' : 'black',
+  //   borderWidth: '2rem',
+  //   border: 'none',
+  //   borderRadius: '100vmax',
+  //   padding: '0.5rem .65rem',
+  //   position: 'absolute',
+  //   top: isMatch ? '0.5rem' : '3.25rem',
+  //   right: '.5rem'
+  // };
 
   return (
     <Button
+      customStyles={`${styles.DarkButton} ${darkMode && styles.DarkMode}`}
       handler={handler}
-      customStyles={customStyles}
       text={
         <FontAwesomeIcon
           icon={faMoon}
