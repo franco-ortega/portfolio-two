63d513e07be48db0a2a54399f79a07aa49fe7c1e
Franco Ortega <francopdx@gmail.com>
2022-11-10 15:30:00 -0800

    create ProjectDetails component

---
+++ components/projects/ProjectDetails.jsx
@@ -0,0 +1,31 @@
+const ProjectDetails = ({
+  title,
+  techList,
+  description,
+  challenges,
+  learnings
+}) => {
+  return (
+    <div data-testid='project-details'>
+      <h2>{title}</h2>
+      <ul>
+        {techList.map((tech, i) => (
+          <li key={i}>{tech}</li>
+        ))}
+      </ul>
+      <p>{description}</p>
+      <ul>
+        {challenges.map((challenge, i) => (
+          <li key={i}>{challenge}</li>
+        ))}
+      </ul>
+      <ul>
+        {learnings.map((learning, i) => (
+          <li key={i}>{learning}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProjectDetails;
