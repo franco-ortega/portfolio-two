92aa24d3a3f6cb052f977d33c74e2b873e7cc13f
Franco Ortega <francopdx@gmail.com>
2022-11-10 14:23:42 -0800

    add testContent to Project testinstead of ProjectsList test

--- components/projects/Project.test.jsx
+++ components/projects/Project.test.jsx
@@ -1,31 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import Project from './Project';
+import testContent from '../../data/testContent.json';
 
 describe('Project component tests', () => {
   it('renders the Project component', () => {
-    const title = 'Title';
-    const techList = ['Tech 1', 'Tech 2', 'Tech 3'];
-    const description =
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates architecto commodi harum accusantium voluptatibus debitis dolor minus quasi molestiae maxime, cum autem id excepturi adipisci.';
-    const challenges = [
-      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere, in.',
-      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere, in.',
-      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere, in.'
-    ];
-    const learnings = [
-      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere, in.',
-      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere, in.',
-      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere, in.'
-    ];
-    render(
-      <Project
-        title={title}
-        techList={techList}
-        description={description}
-        challenges={challenges}
-        learnings={learnings}
-      />
-    );
+    render(<Project {...testContent[0]} />);
 
     const element = screen.getByTestId('project');
 
