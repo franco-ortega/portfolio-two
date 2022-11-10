bf5a9b28e2efc268376d8284ee23cb8e92df64c6
Franco Ortega <francopdx@gmail.com>
2022-11-10 14:17:23 -0800

    add props to Project test

--- components/projects/Project.test.jsx
+++ components/projects/Project.test.jsx
@@ -3,7 +3,29 @@ import Project from './Project';
 
 describe('Project component tests', () => {
   it('renders the Project component', () => {
-    render(<Project />);
+    const title = 'Title';
+    const techList = ['Tech 1', 'Tech 2', 'Tech 3'];
+    const description =
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates architecto commodi harum accusantium voluptatibus debitis dolor minus quasi molestiae maxime, cum autem id excepturi adipisci.';
+    const challenges = [
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere, in.',
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere, in.',
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere, in.'
+    ];
+    const learnings = [
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere, in.',
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere, in.',
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere, in.'
+    ];
+    render(
+      <Project
+        title={title}
+        techList={techList}
+        description={description}
+        challenges={challenges}
+        learnings={learnings}
+      />
+    );
 
     const element = screen.getByTestId('project');
 
