0e15d992866c555def7079dc08e082a880019653
Franco Ortega <francopdx@gmail.com>
2022-11-10 15:21:15 -0800

    update Projects test to receive data prop

--- components/projects/Projects.test.jsx
+++ components/projects/Projects.test.jsx
@@ -1,9 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import Projects from './Projects';
+import testContent from '../../data/testContent.json';
 
 describe('Projects component tests', () => {
   it('renders the Projects component', () => {
-    render(<Projects />);
+    render(<Projects data={testContent} />);
 
     const element = screen.getByTestId('projects');
 
