ef8d49b36b9d98f0393b7d68bcb88429a7738c12
Franco Ortega <francopdx@gmail.com>
2022-11-11 15:29:22 -0800

    console projects data on client

--- components/projects/Projects.jsx
+++ components/projects/Projects.jsx
@@ -1,6 +1,7 @@
 import ProjectsList from './ProjectsList';
 
 const Projects = ({ data }) => {
+  console.log('PROJECTS DATA: ', data);
   return (
     <div data-testid='projects'>
       <h2>Projects Page</h2>
