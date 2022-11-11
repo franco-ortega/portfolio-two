da4939498f6e5d105c67ea2a99013f024491bc60
Franco Ortega <francopdx@gmail.com>
2022-11-10 16:47:33 -0800

    refactor project details handler to explicitly send an object

--- pages/api/projects/[project].js
+++ pages/api/projects/[project].js
@@ -4,10 +4,11 @@ export default async function handler(req, res) {
   if (req.method === 'GET') {
     const response = testContent;
 
-    const [data] = response.filter(
+    const data = response.filter(
       (project) => project.title.toLowerCase() === req.query.project
     );
 
-    res.status(200).json(data);
+    // data is an array, but need to pass a single object to ProjectDetails
+    res.status(200).json(data[0]);
   }
 }
