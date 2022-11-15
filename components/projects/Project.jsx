e4690ed75233914a653e5265bb784db29e596723
Franco Ortega <francopdx@gmail.com>
2022-11-14 16:27:47 -0800

    create initial Project component layout

--- components/projects/Project.jsx
+++ components/projects/Project.jsx
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router';
+import styles from './Project.module.scss';
 
 const Project = ({ title, techList, description }) => {
   const router = useRouter();
@@ -8,15 +9,15 @@ const Project = ({ title, techList, description }) => {
   };
 
   return (
-    <div data-testid='project'>
-      <h2>{title}</h2>
+    <div data-testid='project' className={styles.Project}>
+      <h3>{title}</h3>
       <ul>
         {techList.map((tech, i) => (
           <li key={i}>{tech}</li>
         ))}
       </ul>
       <p>{description}</p>
-      <button onClick={onDetailsClick}>Click for more project details.</button>
+      <button onClick={onDetailsClick}>Click for more details.</button>
     </div>
   );
 };
